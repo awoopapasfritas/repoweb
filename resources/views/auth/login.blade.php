271cb7857950c9003c0417d6d33800f6daabbd93
awoopapasfritas <luca_nata@live.com>
2025-09-25 18:17:17 -0300

    mipr

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,7 @@
       <img src="https://i.kym-cdn.com/entries/icons/original/000/045/723/el_lobo_siempre_cuida_su_loba.jpg" id="icon" alt="User Icon" />
     </div>
  
+
     
     <!-- Login Form -->
     <form action="{{route ('login')}}" method="post">
